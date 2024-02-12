b2337faac813caaba944608dcf9b5198ffc4e59c
RUD-RA <sudharshanbhuyan33@gmail.com>
2024-02-12 21:24:37 +0530

    bugfic

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Connect_to_Database = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URL, {
+    const connect = await mongoose.connect("mongodb+srv://sudarshanbhuyan:zfDYrF0zLtCaGlex@cluster0.21bgwwz.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
